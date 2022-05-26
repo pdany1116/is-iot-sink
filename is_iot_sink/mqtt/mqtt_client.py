f436c929ccee6469c61c18a9733e52b34fa02ffa
Popa Daniel <danypdaniel@gmail.com>
2022-05-27 01:35:40 +0300

    Add authentication for MQTT

--- is_iot_sink/mqtt/mqtt_client.py
+++ is_iot_sink/mqtt/mqtt_client.py
@@ -1,12 +1,16 @@
 from is_iot_sink import utils
 import queue
+import os
 import paho.mqtt.client as mqtt
 from is_iot_sink.logger import LOG
 
 class MQTTClient:
     def __init__(self):
-        self.__host = utils.get_setting("mqtt/host")
         self.__name = utils.get_setting("name")
+        self.__host = os.getenv('MQTT_HOST')
+        self.__port = int(utils.get_setting("mqtt/port"))
+        self.__qos = int(utils.get_setting("mqtt/qos"))
+        self.__auth = utils.get_setting("mqtt/auth")
         self.__registrationTopic = utils.get_setting("mqtt/topics/collector/registration")
         self.__dataTopic = utils.get_setting("mqtt/topics/collector/data")
         self.__valvesTopic = utils.get_setting("mqtt/topics/valves/control")
@@ -16,10 +20,12 @@ class MQTTClient:
         self.__client.on_connect = self.__on_connect
         self.__client.on_disconnect = self.__on_disconnect
         self.__client.on_message = self.__on_message
-        #TODO: use authentication
+            
+        if self.__auth.lower() == "on":
+            self.__client.username_pw_set(os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
 
         try:
-            self.__client.connect(self.__host)
+            self.__client.connect(self.__host, self.__port)
             self.subscribe(self.__registrationTopic)
             self.subscribe(self.__dataTopic)
             self.subscribe(self.__valvesTopic)
@@ -40,7 +46,7 @@ class MQTTClient:
         self.__queue_head = queue_head
 
     def subscribe(self, topic: str):
-        self.__client.subscribe(topic)
+        self.__client.subscribe(topic, self.__qos)
 
     def publish(self, topic: str, message: str):
         try:
@@ -50,7 +56,7 @@ class MQTTClient:
             return
 
         try:
-            self.__client.publish(topic, message)
+            self.__client.publish(topic, message, self.__qos)
         except Exception as ex:
             LOG.err("MQTT Publisher Client failed to publish!")
 
